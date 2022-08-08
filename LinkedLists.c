815f70f295fa88111bfa8b0ab2945551519b94c5
amine759 <saihiamine569@gmail.com>
2022-08-08 14:27:04 +0000

    dix

--- LinkedLists.c
+++ LinkedLists.c
@@ -11,6 +11,11 @@ Node* createNode(int data) ;
 void insert_atHead(Node **head, int data);
 void insert_atTail(Node *head, int data);
 Node* init() ; 
+void insert_given_Index(Node *node, int index, int data);
+void insert_given_Value(Node *node, int value, int data);
+int get_Length(Node *head);
+void delete_head(Node **head);
+
 
 Node* init(){
     Node *head = (Node*)malloc(sizeof(struct Node)) ;
@@ -35,15 +40,15 @@ void insert_atHead(Node **head, int data){
     *head = new_node ;
 }
 
-void insert_atTail(Node *head, int data){
+void insert_atTail(Node *node, int data){
     Node *new_node = createNode(data);
     if (new_node ==NULL){
         printf("\n*** no enough memory ***\n");
     }
-    while(head->next != NULL){
-        head = head->next;   
+    while(node->next != NULL){
+        node = node->next;   
     }
-    head->next = new_node ;
+    node->next = new_node ;
     new_node->next = NULL ;
 }
 
@@ -58,21 +63,116 @@ void printLinkedlist(Node *head){
     printf("\n");
     }
 
-void insert_given_Position(Node *head, int index){
-    
+void insert_given_Index(Node *node, int index, int data){
+    int i = 0 ;
+    Node *new_node = createNode(data);
+    int len = get_Length(node);
+    if (index > len){
+        printf("chosen index out of range ");
+        return ;
+    } 
+    while (node != NULL){
+        i++ ;
+        if(i == index){
+            new_node->next = node->next ; 
+            node->next = new_node ;
+            break;
+        }
+        node = node->next ; 
+    }
 }
-    
+
+void insert_given_Value(Node *node, int value, int data){
+    Node *new_node = createNode(data);
+    while (node != NULL){
+        if(node->data == value){
+            new_node->next = node->next ; 
+            node->next = new_node ;
+            break;
+        }
+        node = node->next ; 
+    }
+}
+
+void delete_given_Value(Node *node, int value){
+
+    while (node->next != NULL){
+        if(node->next->data == value ){
+            Node *temp = node ;     
+            temp = node->next ; 
+            node->next = node->next->next ;
+            free(temp);
+            break;
+        }
+        node = node->next;
+    }
+
+}
+
+void delete_head(Node **head){
+    Node *temp ;
+    if (head == NULL || *head == NULL) return ; 
+
+    temp = *head;
+    *head = (*head)->next;
+
+    free(temp);
+
+}
+
+void delete_given_index(Node *node,int index){
+    int i = 1 ;
+    int len = get_Length(node);
+    if (index > len){
+        printf("chosen index out of range ");
+        return ;
+    }
+        
+    while (node != NULL){
+        if (index == 1){
+            delete_head(&node);
+        }
+        i++ ;
+        if(i == index){
+            Node *temp = node ;
+            temp = node->next ; 
+            node->next = node->next->next ;
+            free(temp);
+            break;
+        }
+        node = node->next;
+    }   
+}   
+
+int get_Length(Node *head){ 
+    int count=0 ; 
+    while( head != NULL ){
+        head = head->next ; 
+        count++ ; 
+    }
+    return count;
+}
+
 
 int main(int argc, char const *argv[])
 {
     Node *head = init();
+
     insert_atHead(&head, 777);
-    insert_atHead(&head, 66);
     insert_atHead(&head, 5);
+    insert_atHead(&head, 1);
+
     insert_atTail(head,8888);
     insert_atTail(head,99999);
-    
+
+    insert_given_Index(head,2,66);    
+    insert_given_Value(head, 8888, 69);
+
+    delete_given_index(head, 1);
+    //delete_given_Value(head, 5);
     printLinkedlist(head);
+
+    free(head);
     return 0;
 }
 
